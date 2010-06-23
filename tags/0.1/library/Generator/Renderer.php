b1d11410187b0d3a8311840a1b565573b1414da0
kobus.jacek <kobus.jacek@45bc48c5-29d2-a2b8-2dbb-f1ba42061f08>
2010-06-23 20:52:11 +0000

    first stable version

---
+++ tags/0.1/library/Generator/Renderer.php
@@ -0,0 +1,132 @@
+<?php
+
+class Generator_Renderer 
+{
+	const TEMPLATE_MODELS 	= './template/model.tpl';
+	const TEMPLATE_TABLES 	= './template/table.tpl';
+	const TEMPLATE_BASE 	= './template/base.tpl';
+	
+	private $_options = array();
+	private $_storage = array();
+	
+	
+	public function __construct(array $options = null)
+	{
+		$this->_options = $options;
+	}
+	
+	protected function store($tableName, $type, $renderedTemplate)
+	{
+		$this->_storage[$type][$tableName] = $renderedTemplate;
+	}
+	
+	public function makeDirectory ($dir)
+	{
+		if (! is_dir($dir)) {
+			mkdir($dir, null, true);
+		}
+	}
+	
+	protected function getOptions()
+	{
+		return $this->_options;
+	}
+	
+	public function make(Generator_Table $table)
+	{
+		// render model
+		$result = $this->render($table, 'model', self::TEMPLATE_MODELS);
+		$this->save($result, $table->getModelFilePath());
+		Generator::log('Saving '.$table->getModelFilePath().'...');
+		// render base
+		
+		// functions
+		
+		$methods = array(
+			'doc' => array('getTable' => $table->getTableName()),
+			'children' => array(),
+			'parents' => array(),
+		);
+		
+		if($children = $table->getChildren()){
+			foreach ($children as $child => $rel){
+				// format children find and create functions
+				$function = $child;
+				// remove table_ prefix if any
+				$pattern = '#^'.$table->getName().'_(.*)$#i';
+				if(preg_match($pattern, $function)){
+					$function = preg_replace($pattern, '\\1', $function);
+				}
+				$function = $table->formatFunctionName($function);
+
+				$methods['children'][] = array(
+					'name' => $child,
+					'function' => $function,
+					'class' => $table->getModelName($child),
+					'table' => $table->getTableName($child),
+					'col' => $rel['col'],
+					'childCol' => $rel['childCol'],
+				);
+			}
+		}
+		
+		if($parents = $table->getParents()){
+			foreach ($parents as $parent => $rel){
+				
+				$methods['parents'][] = array(
+					'name' => $parent,
+					'function' => $table->formatFunctionName($parent),
+					'class' => $table->getModelName($parent),
+					'table' => $table->getTableName($parent)
+				);
+			}
+		}
+		
+		$data['methods'] = $methods;
+		$result = $this->render($table, 'base', self::TEMPLATE_BASE, $data);
+		Generator::log('Saving '.$table->getBaseFilePath().'...');
+		$this->save($result, $table->getBaseFilePath());
+		
+		
+		unset($methods, $data);
+		
+		// render table
+		
+		$result = $this->render($table, 'base', self::TEMPLATE_TABLES);
+		Generator::log('Saving '.$table->getTableFilePath().'...');
+		$this->save($result, $table->getTableFilePath());
+	}
+	
+	/**
+	 * 
+	 * @param Generator_Table $table
+	 * @param string $type model|base|table
+	 * @param string $template file
+	 * @param mix $data optional data passed to view
+	 */
+	public function render($table, $type, $template, $data = null)
+	{
+		ob_start();
+		include $template;
+		$result = ob_get_clean();
+		return $result;
+	}
+	
+	public function save($data, $directory)
+	{
+		file_put_contents($directory, $data);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
